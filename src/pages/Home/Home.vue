a82eec10622a1e74b9d2907e90cce6f992d6cc89
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2025-01-25 20:51:12 +0530

    Crud Operations Practice iInitializaiton using cyferd as databse.

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
+
+<template>
+ <div class="tw-flex tw-justify-center tw-items-center">
+      <div class="tw-w-[600px]"></div>
+ </div>
+</template>
+
+<style scoped></style>
